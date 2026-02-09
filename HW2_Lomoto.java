11443874667b939c894e26d550b8bb0d1e9a5c9f
datvuong194 <vuongquangthanhdat@gmail.com>
2026-02-09 15:00:31 -0800

    Update HW2_Lomoto.java
    Updated the Lomuto partition algorithm implementation and changed the input array.

--- HW2_Lomoto.java
+++ HW2_Lomoto.java
@@ -1,21 +1,23 @@
-// Java program to partition the array
-// using Lomuto Partition Algorithm
+/**
+ * Lomuto Partition Algorithm
+ */ 
 import java.util.Arrays;
 
 public class HW2_Lomoto {
 
     public static void main(String[] args) {
-        int[] arr = {2, 8, 7, 1, 3, 5, 6, 4};
-        partition(arr);
+        int[] arr = {5, 2, 4, 9, 10, 3, 6, 8};
         
-        for (int ele: arr)
-        System.out.print(ele + " ");
+        // Apply Lomoto Partition 
+        lomutoPartition(arr);
+        
+        printArray(arr);
     }
     // Function to partition the array according 
     // to pivot index element
-    static void partition(int[] arr) {
+    static void lomutoPartition(int[] arr) {
         int n = arr.length;
-        int pivot = arr[n - 1];
+        int pivot = arr[n - 1]; // last element
         
         // i acts as boundary between smaller and 
         // larger element compared to pivot
@@ -38,5 +40,10 @@ public class HW2_Lomoto {
         arr[n - 1] = temp;
     }
 
+    private static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
     
 }
