90a8044f73a321fd75e36696349d004131e651da
datvuong194 <vuongquangthanhdat@gmail.com>
2026-02-14 21:24:41 -0800

    Add constructor and toString method to WordFreq

--- WordFreq.java
+++ WordFreq.java
@@ -1,4 +1,14 @@
 class WordFreq {
 	String word;
 	int frequency;
+
+	public WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + word + ", " + frequency + ")";
+    }
 }
