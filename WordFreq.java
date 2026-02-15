4a835ed02e786de7d03ff15eb02ee6cfcb69935a
datvuong194 <vuongquangthanhdat@gmail.com>
2026-02-14 23:18:07 -0800

    Add comments to WordFreq class methods

--- WordFreq.java
+++ WordFreq.java
@@ -1,12 +1,17 @@
+/**
+ * This class is used as the element stored in the max-heap.
+ */ 
+
 class WordFreq {
 	String word;
 	int frequency;
 
+	// Constructor to initialize the phrase and its frequency.
 	public WordFreq(String word, int frequency) {
         this.word = word;
         this.frequency = frequency;
     }
-
+    // Converts the object to a readable string format.
     @Override
     public String toString() {
         return "(" + word + ", " + frequency + ")";
